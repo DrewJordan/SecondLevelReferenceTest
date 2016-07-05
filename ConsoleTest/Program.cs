a802561e2d043395ab30f9a8636efb96c8f9d2c1
Andrew Jordan <ajordan@ksmpartners.com>
2016-07-05 08:34:15 -0400

    Add project files.

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FirstLevel;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var primary = new FirstLevel.FirstLevel();
+            primary.reference.DoSomething();
+        }
+    }
+}
---
+++ FirstReference/FirstLevel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstLevel
+{
+    public class FirstLevel
+    {
+        public SecondLevel.SecondLevel reference;
+
+        public FirstLevel()
+        {
+            reference = new SecondLevel.SecondLevel();
+        }
+    }
+}
